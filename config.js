19158a56e75bc640150fc4393ae8c77190f4da36
KhoaMeow <104348711+Khoameow@users.noreply.github.com>
2024-01-23 13:13:54 +0900

    upload source code

---
+++ config.js
@@ -0,0 +1,30 @@
+//happy Birthday
+var config = {
+  texts: [
+    "Hôm nay là một ngày đặc biệt",
+    "Cô gái dễ thương",
+    "Chúc mừng Sinh Nhật Chị",
+    "Luôn luôn tràn đầy hạnh phúc",
+    "Em mong rằng",
+    "những ước mơ của chị",
+    "sẽ thành hiện thực ",
+    "Mỗi ngày sẽ là một bước tiến mới",
+    "Sinh Nhật Vui Vẻ",
+    "Chị Ạ!",
+    "YAMI~~",
+  ],
+
+  imgs: {
+    "Cô gái dễ thương": "./imgs/images.jpg",
+  },
+  desc: {
+    turn_on: "Click here",
+    play: "Tiếp tục",
+    bannar_coming: "Nhấn tiếp",
+    balloons_flying: "Tiếp tục",
+    cake_fadein: "Nhận tiếp",
+    light_candle: "Click tiếp",
+    wish_message: "Tiếp tục nhấn",
+    story: "Em có đôi lời nhắn với chị",
+  },
+};
